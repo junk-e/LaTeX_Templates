667f9c62ad33173a6d278bfeb8740a18abfd572e
junk-e <jkato0530@gmail.com>
2019-06-28 17:48:56 +0900

    [UPDATE] Commit .gitignore as empty file

--- initrepo.sh
+++ initrepo.sh
@@ -4,9 +4,13 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 if [ ! -f $SCRIPT_DIR/main.tex ]; then
   rm -rf .git
+  cat .gitignore > .gitignore_temp
+  echo > .gitignore
   git init
   git add .gitignore
   git commit -m '[INIT] Initialize repository'
+  cat .gitignore_temp > .gitignore
+  echo initrepo.sh >> .gitignore
   mkdir $SCRIPT_DIR/figure
   mkdir $SCRIPT_DIR/sections
   mv $SCRIPT_DIR/reports/main* $SCRIPT_DIR
